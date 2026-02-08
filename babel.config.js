a46a0c63840794e4f4fd3be9284d6b98483986eb
Selim Saygı <saygiselim@gmail.com>
2026-02-08 19:20:42 +0100

    chore: remove unused dependencies and plugins from configuration files

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Required for TrustChex SDK
-      'react-native-worklets-core/plugin',
-      // Required for react-native-reanimated
-      'react-native-reanimated/plugin',
-    ],
   };
 };
